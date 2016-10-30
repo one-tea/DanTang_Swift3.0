64c9dae9996736f6b2bcd0315c83ad9befa7926a
kevin <zhangkkwy@163.com>
2016-10-30 22:15:52 +0800

    gcd

---
+++ GCD/GCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD
+//
+//  Created by Kevin on 16/10/12.
+//  Copyright © 2016年 zhangkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD/GCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD
+//
+//  Created by Kevin on 16/10/12.
+//  Copyright © 2016年 zhangkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
